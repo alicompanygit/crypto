0575d9f786fe4f616bf1c4e43a4bbc72e9651c00
ali khalaji <167302894+alicompanygit@users.noreply.github.com>
2024-07-27 16:33:12 +0800

    develop

--- src/components/ComponentIs.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <crypto />
-</template>
-<script setup>
-// import MainPageCv from './CvComponents/MainPage.vue'
-import projectOne from './projectOne/MainPage.vue'
-import crypto from './crypto/MainPage.vue'
-</script>
-<style scoped></style>
